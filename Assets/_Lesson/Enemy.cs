b605f2873baf23564b2e6b56bfa239e77ac243f8
teachgame <teachgame@163.com>
2019-07-02 12:11:56 +0800

    Animation Layer and IK Control

---
+++ Assets/_Lesson/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public AnimationCurve ikWeightCurve;
+
+    [Range(0,1)]
+    public float IKWeight;
+    public Transform ikTarget;
+    private Animator anim;
+
+    private Transform playerTrans;
+
+    private Vector3 aimIKPos;
+    private Quaternion aimIKRot;
+
+    public Transform rightHandTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerTrans = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            Shoot();
+            //UseSwitch();
+        }
+
+        Vector3 aimDirection = playerTrans.position - transform.position;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(aimDirection), Time.deltaTime);
+
+        aimIKPos = transform.position + aimDirection.normalized + Vector3.up * 1.4f;
+        IKWeight = anim.GetFloat("AimWeight");
+
+    }
+
+    void UseSwitch()
+    {
+        StartCoroutine(IKCoroutine());
+    }
+
+    void Shoot()
+    {
+        anim.SetBool("Shoot", true);
+    }
+
+    IEnumerator IKCoroutine()
+    {
+        float time = 0;
+
+        while(time <= 1)
+        {
+            time += Time.deltaTime * 0.5f;
+            float weight = ikWeightCurve.Evaluate(time);
+            IKWeight = weight;
+            yield return null;
+        }
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, IKWeight);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, aimIKPos);
+
+        //anim.SetIKRotationWeight(AvatarIKGoal.RightHand, IKWeight);
+        //anim.SetIKRotation(AvatarIKGoal.RightHand, Quaternion.Euler(0,0,0));
+    }
+}
--- Assets/_Lesson/Player.cs
+++ Assets/_Lesson/Player.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public AnimationCurve aimCurve;
+
+    [Range(0, 1)]
+    public float AimIKWeight;
+    public Transform AimIKTarget;
+
+    [Range(0, 1)]
+    public float FootIKWeight;
+
+    public Transform hipTrans;
+
+    private Vector3 leftFootIKPos;
+    private Vector3 rightFootIKPos;
+
+    private Vector3 leftFootIKNormal;
+    private Vector3 rightFootIKNormal;
+
     [Range(0, 720)]
     public float rotateSpeed = 180;
     private Transform camTrans;
@@ -11,6 +28,10 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
     private bool isSneaking;
+
+    public Transform gunTrans;
+    public Transform gunHolderTrans;
+    public Transform gunAimPosTrans;
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +47,31 @@ public class Player : MonoBehaviour
         float v = Input.GetAxis("Vertical");
 
         isWalking = Input.GetKey(KeyCode.LeftShift);
-        
-        if(Input.GetKeyDown(KeyCode.C))
+
+        leftFootIKPos = GetFootIKPos(transform, transform.up + transform.right * -0.2f, out leftFootIKNormal) + Vector3.up * 0.1f;
+        rightFootIKPos = GetFootIKPos(transform, transform.up + transform.right * 0.2f, out rightFootIKNormal) + Vector3.up * 0.1f;
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            StartAim();
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            EndAim();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
         {
             isSneaking = !isSneaking;
             anim.SetBool("Sneak", isSneaking);
         }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            anim.SetTrigger("Shout");
+        }
+
 
         Vector3 camTransForwardProj = camTrans.forward;
         camTransForwardProj.Set(camTransForwardProj.x, 0, camTransForwardProj.z);
@@ -50,4 +90,111 @@ public class Player : MonoBehaviour
         //anim.SetFloat("Speed", v);
         anim.SetFloat("Speed", directionMag, 0.25f, Time.deltaTime);
     }
+
+    Vector3 GetFootIKPos(Transform hipTrans, Vector3 offset, out Vector3 normal)
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(hipTrans.position + offset, Vector3.down, out hit, 1.1f))
+        {
+            normal = hit.normal;
+            FootIKWeight = 1;
+            return hit.point;
+        }
+        normal = Vector3.zero;
+        FootIKWeight = 0;
+        return hit.point;
+    }
+
+    void StartAim()
+    {
+        StartCoroutine(AimCoroutine());
+    }
+
+    void EndAim()
+    {
+        StartCoroutine(EndAimCoroutine());
+    }
+
+    IEnumerator AimCoroutine()
+    {
+        float time = 0;
+
+        while(time < 1)
+        {
+            time += Time.deltaTime * 4;
+            AimIKWeight = aimCurve.Evaluate(time);
+            yield return null;
+        }
+
+        time = 0;
+
+        while(time < 1)
+        {
+            time += Time.deltaTime * 4;
+
+            Vector3 gunPos = Vector3.Lerp(gunHolderTrans.position, gunAimPosTrans.position, aimCurve.Evaluate(time));
+            Quaternion gunRot = Quaternion.Lerp(gunHolderTrans.rotation, gunAimPosTrans.rotation, aimCurve.Evaluate(time));
+
+            gunTrans.position = gunPos;
+            gunTrans.rotation = gunRot;
+            yield return null;
+        }
+    }
+
+    IEnumerator EndAimCoroutine()
+    {
+        float time = 1;
+
+        while (time > 0)
+        {
+            time -= Time.deltaTime * 4;
+
+            Vector3 gunPos = Vector3.Lerp(gunHolderTrans.position, gunAimPosTrans.position, aimCurve.Evaluate(time));
+            Quaternion gunRot = Quaternion.Lerp(gunHolderTrans.rotation, gunAimPosTrans.rotation, aimCurve.Evaluate(time));
+
+            gunTrans.position = gunPos;
+            gunTrans.rotation = gunRot;
+            yield return null;
+        }
+
+        time = 1;
+
+        while (time > 0)
+        {
+            time -= Time.deltaTime * 4;
+            AimIKWeight = aimCurve.Evaluate(time);
+            yield return null;
+        }
+    }
+
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, AimIKWeight);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, AimIKTarget.position);
+
+        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, AimIKWeight);
+        anim.SetIKRotation(AvatarIKGoal.RightHand, AimIKTarget.rotation);
+
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, FootIKWeight);
+        anim.SetIKPosition(AvatarIKGoal.LeftFoot, leftFootIKPos);
+
+        anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, FootIKWeight);
+        anim.SetIKPosition(AvatarIKGoal.RightFoot, rightFootIKPos);
+
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, AimIKWeight);
+        anim.SetIKRotation(AvatarIKGoal.LeftFoot, Quaternion.FromToRotation(Vector3.right, leftFootIKNormal));
+
+        anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, AimIKWeight);
+        anim.SetIKRotation(AvatarIKGoal.RightFoot, Quaternion.FromToRotation(Vector3.right, rightFootIKNormal));
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(leftFootIKPos, 0.1f);
+        Gizmos.DrawWireSphere(rightFootIKPos, 0.1f);
+    }
+
 }
